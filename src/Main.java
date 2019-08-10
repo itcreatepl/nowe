06d5640fb6935088499c5d77d8cc38c1f3307ba7
Piotr Hołownia <biuro@itcreate.pl>
2019-08-10 14:34:58 +0200

    Main class addeed list

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        List<Cat> catList = new ArrayList<>();
+        catList.add(new Cat("kot1",1));
+        catList.add(new Cat("kot2",1));
+        catList.add(new Cat("kot3",1));
+        catList.add(new Cat("kot4",1));
     }
 
 }
